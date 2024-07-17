24628fd33d601202ecd8da4abdabbf165920a17f
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-16 23:31:47 -1000

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,16 @@
+// Basic Calculator Functions
+function addNum(firstVal, secondVal) {
+    return firstVal + secondVal;
+};
+
+function subtractNum(firstVal, secondVal) {
+    return firstVal - secondVal;
+}
+
+function multiplyNum(firstVal, secondVal) {
+    return firstVal * secondVal;
+}
+
+function divideNum(firstVal, secondVal) {
+    return firstVal / secondVal;
+};
