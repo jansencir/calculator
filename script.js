a366eae04be1e1091c2e700b89a505d938ac14bd
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-19 01:07:26 -1000

    Add notes to script.js
    What I want the calculator to do

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function multiplyNum(firstVal, secondVal) {
 function divideNum(firstVal, secondVal) {
     return firstVal / secondVal;
 };
+
+
+/* What I want the calculator to do
+-Display
+    -2 rows in the display:
+        -Top row: shows the current 2 number operation
+        -Second row: what the tow row comes out to (after hitting equal button)
+    -When hitting clear:
+        -If there is an equation with no answer, it clear the second number in the operation
+        -At that point, you should be able to change the operator too
+    Only hit all clear when there is nothing to clear
+        -Resets all operations (clear history)
+
+-Clear/All Clear
+    -Switches depending on what is in the first row
+
+-MAYBE
+    LEFT SIDE
+    -Make a history table of operations on the side
+        -New operations are at the bottom, and older ones get pushed up
+        -Should delete past operations after (maybe?) 12
+        -Buttons: Remember & Delete
+    RIGHT SIDE
+    -Each past operation on the left can have a "remember button"
+        -If you click the "remember button":
+            -Creates a new list on the right with the numbers that need to be remembered
+        -New items are at the bottom, and older ones get pushed up
+        -Buttons: Delete
+*/
