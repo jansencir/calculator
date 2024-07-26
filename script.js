4ef3fc8a0386baaa41880490fd634d6f5df5371a
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-26 01:06:42 -1000

    Add variables to script.js

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ function divideNum(firstVal, secondVal) {
 };
 
 // Variable for Three Parts
-let numberOne;
-let numberOperator;
-let numberTwo;
+let numberOne = "";
+let numberOperator = "";
+let numberTwo = "";
+let previousDisplay = "";
+let currentDisplay = "";
 
 const calculatorOperators = document.querySelectorAll(".anOperator");
 const calculatorNumbers = document.querySelectorAll(".aNumber");
@@ -26,6 +28,8 @@ const calculatorDecimal = document.querySelector("#decimalBtn");
 const calculatorPositiveNegative = document.querySelector("#positiveNegative");
 const calculatorClear = document.querySelector("#clearBtn");
 const calculatorEqual = document.querySelector("#equalBtn");
+const calculatorPreviousDisplay = document.querySelector("#previous-display");
+const calculatorCurrentDisplay = document.querySelector("#current-display");
 
 calculatorOperators.forEach(operator => {
     operator.addEventListener("click", () => console.log("operator clicked"))
@@ -42,3 +46,16 @@ calculatorPositiveNegative.addEventListener("click", () => console.log("positive
 calculatorClear.addEventListener("click", () => console.log("clear clicked"))
 
 calculatorEqual.addEventListener("click", () => console.log("equal clicked"))
+
+// calculatorCurrentDisplay.textContent = currentDisplay;
+
+// Calculator Functions
+/* Update Display
+*/
+/* Push Number to Variables
+    -If 
+    -When a number button is pushed, add the number to 
+*/
+// Clear Button & All Clear Button
+// Compute Current Function
+// Switch Sign
