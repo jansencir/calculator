96db22204ae6cbae8425c23fc51ed85d95357a79
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-25 19:53:39 -1000

    Add event listeners to buttons
    Each should log what they are in the console super basic

--- script.js
+++ script.js
@@ -20,7 +20,25 @@ let numberOne;
 let numberOperator;
 let numberTwo;
 
-function operate(anOperator, num1, num2) {
-    return anOperator(num1, num2);
-};
+const calculatorOperators = document.querySelectorAll(".anOperator");
+const calculatorNumbers = document.querySelectorAll(".aNumber");
+const calculatorDecimal = document.querySelector("#decimalBtn");
+const calculatorPositiveNegative = document.querySelector("#positiveNegative");
+const calculatorClear = document.querySelector("#clearBtn");
+const calculatorEqual = document.querySelector("#equalBtn");
 
+calculatorOperators.forEach(operator => {
+    operator.addEventListener("click", () => console.log("operator clicked"))
+})
+
+calculatorNumbers.forEach(number => {
+    number.addEventListener("click", () => console.log("number clicked"))
+})
+
+calculatorDecimal.addEventListener("click", () => console.log("decimal clicked"))
+
+calculatorPositiveNegative.addEventListener("click", () => console.log("positive/negative clicked"))
+
+calculatorClear.addEventListener("click", () => console.log("clear clicked"))
+
+calculatorEqual.addEventListener("click", () => console.log("equal clicked"))
