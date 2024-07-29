2e83be8fad780388af0b22ea5088eac12d511bda
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-28 20:19:06 -1000

    Add clear function and delete dead code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let numberOne = "";
 let numberOperator = "";
 let numberTwo = "";
-let previousDisplay = "";
-let currentDisplay = "";
 
 const calculate = (one, op, two) => {
     let first = parseFloat(one)
@@ -14,12 +12,6 @@ const calculate = (one, op, two) => {
         case "÷": return first / second;
     }
 }
-// console.log(calculate(numberOne, numberOperator, numberTwo));
-/* TESTERS
-let numberOne = "3";
-let numberOperator = "+";
-let numberTwo = "3";
-*/
 
 const appendNumber = (number) => {
     // click needs to pass number
@@ -32,10 +24,8 @@ const appendNumber = (number) => {
 }
 
 const chooseOperator = (operator) => {
-    if (numberOperator != "") return
-    if (numberOperator == "") {
-        numberOperator += operator.innerText
-    }
+    if (numberOperator != "" || numberOne == "") return
+    else {numberOperator += operator.innerText}
 }
 
 
@@ -43,21 +33,21 @@ const chooseOperator = (operator) => {
 // make two new functions, positiveToNegative and negativeToPositive
 // if numberOne or numberTwo is positive, run positiveToNegative
 // if numberOne or numberTwo is NEGATIVE, run negativeToPositive
-const changeSign = () => {
-    // positive to negative
-    if (numberOperator == "" && numberOne.charAt(0) != "-") {
-        numberOne = "-" + numberOne
-    }
-    if (numberOperator != "" && numberTwo.charAt(0) != "-") {
-        numberTwo = "-" + numberTwo
-    }
-    // negative to positive
-    if (numberOperator == "" && numberOne.charAt(0) == "-") {
-        numberOne = numberOne.split("").splice(1).join("")
-    }
-    if (numberOperator != "" && numberTwo.charAt(0) == "-")
-        numberTwo.split("").splice(1).join("")
-}
+// const changeSign = () => {
+//     // positive to negative
+//     if (numberOperator == "" && numberOne.charAt(0) != "-") {
+//         numberOne = "-" + numberOne
+//     }
+//     if (numberOperator != "" && numberTwo.charAt(0) != "-") {
+//         numberTwo = "-" + numberTwo
+//     }
+//     // negative to positive
+//     if (numberOperator == "" && numberOne.charAt(0) == "-") {
+//         numberOne = numberOne.split("").splice(1).join("")
+//     }
+//     if (numberOperator != "" && numberTwo.charAt(0) == "-")
+//         numberTwo.split("").splice(1).join("")
+// }
 
 
 // GOOD positive to negative
@@ -78,11 +68,6 @@ function checkSign(str) {
     console.log(str)
 }
 
-/* UPDATE DISPLAY
-    -if numberOperator == "", currentDisplay = numberOne
-    -if numberOperator != "", previousDisplay = `numberOne numberOperator` ; currentDisplay = `numberTwo`
-
-*/
 const updateDisplay = () => {
     if (numberOperator == "") {
         calculatorCurrentDisplay.textContent = `${numberOne}`
@@ -93,6 +78,27 @@ const updateDisplay = () => {
     }
 }
 
+// CLEAR ALL CLEAR DISPLAY
+const clearDisplay = () => {
+    calculatorCurrentDisplay.textContent = "";
+    numberTwo = "";
+}
+
+const clearAllDisplay = () => {
+    calculatorPreviousDisplay.textContent = "";
+    calculatorCurrentDisplay.textContent = "";
+    numberOne = "";
+    numberOperator = "";
+    numberTwo = "";
+}
+
+const clearTextDisplay = () => {
+    if (calculatorPreviousDisplay.innerText != "" && calculatorCurrentDisplay.innerText != "") {
+        calculatorClear.innerText = "CLR"
+    } else {
+        calculatorClear.innerText = "ALL CLR"
+    }
+}
 
 const calculatorOperators = document.querySelectorAll(".anOperator");
 const calculatorNumbers = document.querySelectorAll(".aNumber");
@@ -107,64 +113,31 @@ calculatorOperators.forEach(operator => {
     operator.addEventListener("click", () => {
         chooseOperator(operator)
         updateDisplay()
+        clearTextDisplay()
     })
 })
-
-// calculatorNumbers.forEach(number => {
-//     number.addEventListener("click", () => console.log("number clicked"))
-// })
 
 calculatorDecimal.addEventListener("click", () => console.log("decimal clicked"))
 
 calculatorPositiveNegative.addEventListener("click", () => changeSign())
-// calculatorPositiveNegative.addEventListener("click", () => console.log("positive/negative clicked"))
 
-calculatorClear.addEventListener("click", () => console.log("clear clicked"))
+
+calculatorClear.addEventListener("click", () => {
+    if (calculatorClear.innerText == "CLR") {clearDisplay()}
+    if (calculatorClear.innerText == "ALL CLR") {clearAllDisplay()}
+    console.log("clear clicked")
+    updateDisplay()
+    clearTextDisplay()
+})
 
 calculatorEqual.addEventListener("click", () => console.log("equal clicked"))
-
-// calculatorCurrentDisplay.textContent = currentDisplay;
-
-// Calculator Functions
-/* Update Display
-*/
-/* Push Number to Variables
-    -When a number button is clicked, add the number to the end of the string
-*/
-// Clear Button & All Clear Button
-// Compute Current Function
-// Switch Sign
-
-// --------------------------------------------------
-// calculatorNumbers.forEach(number => {
-//     number.addEventListener("click", () => {
-//         numberOne += number.innerText
-//         calculatorCurrentDisplay.textContent = numberOne
-//     })
-// })
 
 calculatorNumbers.forEach(number => {
     number.addEventListener("click", () => {
         appendNumber(number)
         updateDisplay()
+        clearTextDisplay()
     })
 })
-// --------------------------------------------------
 
-// REMAP EVERYTHING AND CLEAN UP A LITTLE
-// FIGURE OUT HOW TO LINK SPECIFIC FUNCTIONS TO EVENT LISTENERS
-// FUNCTIONS GO ON TOP, CALLS GO ON THE BOTTOM
-/* How the number variables SHOULD work
-    -for operation reasons
-        -numberOne takes the first number before an operator is clicked
-        -numberOperator takes whatever operator was clicked
-        -numberTwo takes the number after it's clicked
-    -for display reasons
-        -at first, the previous display takes numberOne, the operator, and numberTwo
-        -once equals is clicked
-            -it shows the answer in current
-            -if an operator is clicked after, it gets rid of the previousDisplay, pushes the previous answer, the operator, and the second number
-    -for linking reasons
-        -the answer to the previous operation is pushed to the previousDisplay
-        -
-*/
+clearTextDisplay()
